b35599a1e552a64669fa2d777b33a512a750fd5c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-22 21:16:09 +0600

    added

--- src/components/ErrorPage/ErrorPage .jsx
+++ src/components/ErrorPage/ErrorPage .jsx
@@ -1,17 +1,18 @@
     import React from "react";
     import { Link } from "react-router-dom";
+    import ErrorImage from "../../assets/image/error-404.png"
 
     const ErrorPage = () => {
     return (
         <div className="h-screen flex flex-col justify-center items-center text-center">
-        <h1 className="text-6xl font-bold text-red-500">404</h1>
-        <p className="text-xl mt-4">Oops! Page Not Found</p>
-
-        <Link to="/">
-            <button className="mt-6 px-6 py-2 bg-blue-500 text-white rounded">
-            Go Home
-            </button>
-        </Link>
+            <img src={ErrorImage} alt="Error 404" />
+            <h1 className="text-6xl font-bold text-red-500">Oops! Page Not Found</h1>
+            
+            <Link to="/">
+                <button className="mt-6 px-6 py-2 bg-blue-500 text-white rounded">
+                    Go Home
+                </button>
+            </Link>
         </div>
     );
     };
